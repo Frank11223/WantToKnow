d0ae8b4d00c99e754c4b1817da13e831b45b451e
frank <206016050@qq.com>
2023-05-05 23:05:40 +0800

    js原型

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,8 @@ module.exports = {
             ['js_data_type', '数据类型'],
             ['browser', '浏览器渲染原理'],
             ['event_loop', '事件循环'],
-            ['js_typeof_instanceof', 'type与instanceof区别']
+            ['js_typeof_instanceof', 'type与instanceof区别'],
+            ['js_prototype', 'js原型']
           ],
         },
       ],
